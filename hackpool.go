30659d74fba0a58ba0e5a38fae4dfb9f35e32986
lgq12172009 <lgq.1217@163.com>
2018-07-16 19:00:24 +0800

    update run function

--- hackpool.go
+++ hackpool.go
@@ -5,17 +5,15 @@ import (
 )
 
 type HackPool struct {
-	state    chan struct{}
-	messages chan interface{}
 	numGo    int
+	messages chan interface{}
 	function func(interface{})
 }
 
 func New(numGoroutine int, function func(interface{})) *HackPool {
 	return &HackPool{
-		state:    make(chan struct{}, numGoroutine),
-		messages: make(chan interface{}, numGoroutine),
 		numGo:    numGoroutine,
+		messages: make(chan interface{}, numGoroutine),
 		function: function,
 	}
 }
@@ -33,26 +31,17 @@ func (c *HackPool) CloseQueue() {
 }
 
 func (c *HackPool) Run() {
-
 	var wg sync.WaitGroup
 
-	// 阻塞,等待message有数据或close
-	for v := range c.messages {
+	wg.Add(c.numGo)
 
-		// 增加state的目的是为了控制同时执行的协程数量
-		c.state <- struct{}{}
-
-		wg.Add(1)
-
-		go func(value interface{}) {
-
-			c.function(value)
-
-			// 协程执行完毕, 下一个协程才能启动
-			<-c.state
-
+	for i := 0; i < c.numGo; i++ {
+		go func() {
+			for v := range c.messages {
+				c.function(v)
+			}
 			wg.Done()
-		}(v)
+		}()
 	}
 
 	wg.Wait()
